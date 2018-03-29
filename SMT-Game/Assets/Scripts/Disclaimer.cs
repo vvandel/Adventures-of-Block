9435338f6fe31e68dce4e605fc1b0ffe4ad1fc72
vvandel <victorvanandel@hotmail.com>
2018-03-29 15:28:23 +0200

    Disclaimer scene
    Added the disclaimer scene (screen with terms & conditions for the
    research with a checkbox and a button to continue)

---
+++ SMT-Game/Assets/Scripts/Disclaimer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System;
+
+public class Disclaimer : MonoBehaviour {
+
+    public void OnButton()
+    {
+        Toggle agreeToggle = FindObjectOfType<Toggle>();
+        if (agreeToggle.isOn){
+            SceneManager.LoadScene("menu");
+        }
+    }
+}
